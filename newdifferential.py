25e9ac27db011652bc058559c82688302477caf4
hoho <chrisdodd@uvic.ca>
2020-07-09 22:21:48 -0700

    Update newdifferential.py

--- newdifferential.py
+++ newdifferential.py
@@ -10,16 +10,16 @@ import Adafruit_MCP3008
 
 
 # Software SPI configuration:
-# CLK  = 18
-# MISO = 23
-# MOSI = 24
-# CS   = 25
-# mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 25
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 # Hardware SPI configuration:
-SPI_PORT   = 0
-SPI_DEVICE = 0
-mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+#SPI_PORT   = 0
+#SPI_DEVICE = 0
+#mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 print('Press Ctrl-C to quit...')
 while True:
