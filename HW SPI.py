eda4e8f0f5e83385cfa6e951ad59d63bdad28b49
hoho <chrisdodd@uvic.ca>
2020-07-10 19:59:23 -0700

    Update and rename FUCK.py to HW SPI.py

--- HW SPI.py
+++ HW SPI.py
@@ -18,10 +18,10 @@ SPI_TYPE = 'HW'
 dly = .5         # Delay of 1000ms (1 second)
 
 # Software SPI Configuration
-CLK     = 18    # Set the Serial Clock pin
-MISO    = 23    # Set the Master Input/Slave Output pin
-MOSI    = 24    # Set the Master Output/Slave Input pin
-CS      = 25    # Set the Slave Select
+#CLK     = 18    # Set the Serial Clock pin
+#MISO    = 23    # Set the Master Input/Slave Output pin
+#MOSI    = 24    # Set the Master Output/Slave Input pin
+#CS      = 25    # Set the Slave Select
 
 # Hardware SPI Configuration
 HW_SPI_PORT = 0 # Set the SPI Port. Raspi has two.
