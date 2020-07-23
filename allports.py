b951027efcf72ff7f7e61cc5ae85770377ea5b78
hoho <chrisdodd@uvic.ca>
2020-07-23 11:43:46 -0700

    Update allports.py

--- allports.py
+++ allports.py
@@ -16,11 +16,37 @@ import time
 import datetime
 from firebase import firebase
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': plantmonitor-723b4,
+})
+
+db = firestore.client()
+
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1815
+})
+
+doc_ref = db.collection(u'users').document(u'aturing')
+doc_ref.set({
+    u'first': u'Alan',
+    u'middle': u'Mathison',
+    u'last': u'Turing',
+    u'born': 1912
+})
 
 
 
-firebase = firebase.FirebaseApplication('plantmonitor-723b4',None)
-firebase.put('plantmonitor-723b4', 'Please Work', 'please the lord')
+# firebase = firebase.FirebaseApplication('plantmonitor-723b4',None)
+# firebase.put('plantmonitor-723b4', 'Please Work', 'please the lord')
 
 # Software SPI configuration:
 CLK  = 18
