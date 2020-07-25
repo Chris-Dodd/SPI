800c3b760a5033b5b3668938eb1b2a03a42dab6e
hoho <chrisdodd@uvic.ca>
2020-07-24 21:29:25 -0700

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -7,8 +7,11 @@ app = firebase_admin.initialize_app(cred)
 
 store = firestore.client()
 doc_ref = store.collection(u'plant_data').document('plant_0')
+doc_ref2 = store.collection(u'plant_data').document('plant_1')
+
 
 doc_ref.set({u'humidity': 109})
+doc_ref2.set({u'test': 209})
 try:
     docs = doc_ref.get()
     print(u'Doc Data:{}'.format(docs.to_dict()))
