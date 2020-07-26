ba5de3f43ce59f27d2106c6d4fdd9eaa26848637
hoho <chrisdodd@uvic.ca>
2020-07-26 11:16:13 -0700

    Update 499_ADC_Final.py

--- 499_ADC_Final.py
+++ 499_ADC_Final.py
@@ -126,10 +126,9 @@ while True:
             #Will scale using: T = (Tmax - Tmin) * Tread / 1023), for 10 bit read_adc
             vt0 = values[i] *[(100)/1023]
             doc_ref0.set({u'humidity': vt0})
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+            print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
             # Pause for half a second.
-
-    time.sleep(2)
+            time.sleep(2)
             '''
         if(i==1): #Moisture for plant0
          #The Moisture sensor range is from 0 - 100
